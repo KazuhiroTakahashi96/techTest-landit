6899360b02e010408c49fe41c5db544770364d41
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-26 23:51:36 +0900

    Estate Transaction Contextファイル

---
+++ src/context/useEstateTransactionContext.ts
@@ -0,0 +1,6 @@
+import { useContext } from "react";
+import { EstateTransactionContext } from "../providers/EstateTransactionProvider";
+
+export const useEstateTransactionContext = () => {
+  return useContext(EstateTransactionContext);
+};
