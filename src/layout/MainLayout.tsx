f4f2e195cd1966ee2e7996340e3da69d5997048a
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-26 23:50:17 +0900

    main layout

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,19 @@
+import Content from "../components/Content";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const MainLayout = ({ children }: Props) => {
+  return (
+    <>
+      <Header />
+      <Content>{children}</Content>
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
