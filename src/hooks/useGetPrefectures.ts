a98893ace3e7f11c521b30ca9d299bf36603d410
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-26 23:52:33 +0900

    customHook: 都道府県データ取得

---
+++ src/hooks/useGetPrefectures.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+export type Prefectures = {
+  prefCode: number;
+  prefName: string;
+};
+
+const useGetPrefectures = () => {
+  const [prefectures, setPrefectures] = useState<Prefectures[]>([]);
+
+  useEffect(() => {
+    const fetchPrefectures = async () => {
+      const res = await fetch(
+        "https://opendata.resas-portal.go.jp/api/v1/prefectures",
+        {
+          headers: {
+            "X-API-KEY": import.meta.env.VITE_RESAS_API_KEY,
+          },
+        }
+      );
+
+      const data = await res.json();
+      // console.log(data);
+      setPrefectures(data.result);
+    };
+    fetchPrefectures();
+  }, []);
+
+  return prefectures;
+};
+export default useGetPrefectures;
