d1fccd02645ed14abc2a3af1c43c0ff68cd73aa4
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-27 23:49:17 +0900

    radio button component

---
+++ src/components/Radio.tsx
@@ -0,0 +1,26 @@
+type Props = {
+  label: string;
+  value: string | number;
+  name: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Radio = ({ label, value, name, onChange }: Props) => {
+  return (
+    <div>
+      <label htmlFor={label} className="flex items-center cursor-pointer">
+        <input
+          type="radio"
+          name={name}
+          id={label}
+          value={value}
+          onChange={onChange}
+          className="rounded-full w-6 h-6 border border-text-link"
+        />
+        <span className="ml-2 text-sm text-text-black">{label}</span>
+      </label>
+    </div>
+  );
+};
+
+export default Radio;
