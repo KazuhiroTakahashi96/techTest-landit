7b250ca822b0281903069b75cd2fbb2e66097b02
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-27 23:54:38 +0900

    estate transaction page

---
+++ src/pages/estateTransaction/Index.tsx
@@ -0,0 +1,25 @@
+import MainLayout from "../../layout/MainLayout";
+import Form from "../../components/form/Form";
+import Title from "../../components/Title";
+import Chart from "../../components/chart/Chart";
+
+import Graph from "../../assets/icons/graph.svg";
+
+const EstateTransaction = () => {
+  return (
+    <MainLayout>
+      <Title label="取引価格" subtitle="※取引面積1㎡あたり">
+        <div className="py-[5px] mr-2.5">
+          <img src={Graph} alt="" className="w-[30px] h-[30px]" />
+        </div>
+      </Title>
+
+      <div className="w-[1840px] h-full flex gap-6 py-10">
+        <Chart />
+        <Form />
+      </div>
+    </MainLayout>
+  );
+};
+
+export default EstateTransaction;
