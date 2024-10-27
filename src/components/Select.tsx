9aad9e05e46f5c2d268c3d530375a1df7821b8c9
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-27 23:49:37 +0900

    select component

---
+++ src/components/Select.tsx
@@ -0,0 +1,26 @@
+type Props = {
+  label?: string;
+  name: string;
+  children?: React.ReactNode;
+  options: { value: string | number; label: string }[];
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+
+const Select = ({ label, name, children, options, onChange }: Props) => {
+  return (
+    <div className="flex items-center justify-center h-full w-full">
+      {children}
+      {label ? <p className="">{label}</p> : null}
+      <select name={name} className="w-full h-full pl-2" onChange={onChange}>
+        <option>選択してください</option>
+        {options.map((opt) => (
+          <option key={opt.label} value={opt.value}>
+            {opt.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
